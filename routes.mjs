2b2a2912e288623dd28d830e56d763ba0d193cda
unknown <quahzhengjie@gmail.com>
2022-05-19 03:34:01 +0800

    testing git commit

--- routes.mjs
+++ routes.mjs
@@ -4,4 +4,24 @@ export default function routes(app) {
   app.get('/', (request, response) => {
     response.sendFile(resolve('dist', 'main.html'));
   });
+  //ROOT
+  //Log in page
+  //Register Page
+  //
+  //Jobs Controller (MiddleWare Required)
+  //Add Jobs
+  //Delete Jobs
+  //Edit Jobs
+  //Single Job 
+  //MANY JOB FROM 1 COMPANY
+  //
+  // User Controller 
+  // Resume (Template)
+  // Profile (form)
+  // Industry
+  // Skills
+  // Social Media
+  // Arrange Interview
+  // Put on ALERT if hiring
+
 }
